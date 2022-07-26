d332009b3f46a4658b614179a35740828a68802b
Jotaroo <77038542+2668342956@users.noreply.github.com>
2022-07-26 11:26:00 +0800

    Create 2019C-爬取成都双流机场数据.py
    双流机场网站应该有对爬虫的限制，所以一个for循环爬取不了所有的数据，建议是在Jupyter环境下，将for循环单独放在一个Ceil里面运行，运行一次后看一下爬取的数据，然后改变for的起始range序号，两次爬取建议间隔一两分钟，多循环几次应该就可以爬取下某一天的全部数据

---
+++ 2019C-爬取成都双流机场数据.py
@@ -0,0 +1,23 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+url = 'https://www.cdairport.com/dynamic3.aspx'
+path = r'D:\Learning\数模\2022高教杯\第三次培训\数据\airplane.xlsx' # 将此路径改为你的本地路径
+headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.71'}
+Info = []
+for i in range(1,42):
+    params = {
+    't':'8',
+    'inout':'a',
+    'date':'-1',
+    'etime':'23:59',
+    'page':str(i)
+    }
+    res = requests.get(url,params=params)
+    sp = BeautifulSoup(res.text,'html.parser')
+    flights = sp.find_all('li',class_='clearfix')
+    for flight in flights:
+        Zip = flight.find_all('span',class_='dltxt')
+        Info.append([Zip[i].text for i in range(len(Zip))])
+Info = pd.DataFrame(Info,columns=['航班号','始发地','目的地','经停','航空公司','计划到达','航站楼','状态'])
+Info.to_excel(path,index=False)
